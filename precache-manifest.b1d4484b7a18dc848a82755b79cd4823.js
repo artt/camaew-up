117405023f9e6ed4d844d1623b4bf895aa569cdc
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-07-25 12:19:29 +0700

    Updates

---
+++ precache-manifest.b1d4484b7a18dc848a82755b79cd4823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e8421c3a4608789a56d9c55c841ce4",
+    "url": "/camaew-up/index.html"
+  },
+  {
+    "revision": "f735261939d423675e15",
+    "url": "/camaew-up/static/css/main.e2c8c498.chunk.css"
+  },
+  {
+    "revision": "602e206166de54d1b3c0",
+    "url": "/camaew-up/static/js/2.1ba85bae.chunk.js"
+  },
+  {
+    "revision": "b6f4b22afc5b6bdf8334e3ee71c0d9d4",
+    "url": "/camaew-up/static/js/2.1ba85bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f735261939d423675e15",
+    "url": "/camaew-up/static/js/main.7ca211dd.chunk.js"
+  },
+  {
+    "revision": "9d96e7a8564989304617",
+    "url": "/camaew-up/static/js/runtime-main.4820c58b.js"
+  }
+]);
